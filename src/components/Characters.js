7374e423c4086160894c01bc2109dcaf3c153574
longpn0504 <longpn0504@gmail.com>
2023-09-27 20:05:09 -0700

    characters page

--- src/components/Characters.js
+++ src/components/Characters.js
@@ -1,22 +1,56 @@
 //Characters page
 import React from 'react';
+import exampleImage from '../images/example.png';
 
 function Characters() {
+  const characters = [
+    {
+      name: 'Fly Bronze',
+      description: 'description about this character',
+      image: exampleImage
+    },
+    {
+      name: 'Chirster',
+      description: 'description about this character',
+      image: exampleImage
+    },
+    {
+      name: 'Mimia Man',
+      description: 'description about this character',
+      image: exampleImage
+    },
+    {
+      name: 'Wonder Wrath',
+      description: 'description about this character',
+      image: exampleImage
+    },
+    {
+      name: 'Jairan',
+      description: 'description about this character',
+      image: exampleImage
+    },
+    {
+      name: 'Human Strike',
+      description: 'description about this character',
+      image: exampleImage
+    },
+
+  ]
   return (
-    <div className='text-white'>
-      <h1>Characters</h1>
-      <br />
-
-      <p>Potential Character List      </p>
-
-        <br />
-
-          <li>Fly Bronze </li>
-          <li>Christer</li>
-          <li>Mimia Man</li>
-          <li>Wonder Wrath</li>
-          <li>Jairan </li>
-          <li>Human Strike</li>
+    <div className='text-white text-center'>
+      <h1 className='text-3xl font-thin'>Choose your</h1>
+      <h1 className='text-5xl font-serif italic'>Character</h1>
+      {characters.map((character,index) => (
+          <div key={index} className="smb-10 sm:mb-0 mt-10 grid gap-4 grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
+          <div className="flex justify-center bg-gray-900 py-10 px-10 space-y-2">
+            <img className="w-40 h-40 rounded-full object-cover object-center" src={character.image} />
+            <div className="relative w-[60%] py-2 px-10"> 
+              <h1 className="text-white text-2xl font-bold capitalize text-center">{character.name}</h1>
+              <p>{character.description}</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,14 +12,14 @@ export default function Narbar()
 
     return(
         <div className="text-white flex justify-between items-center h-24  mx-auto px-4 max-w-[1024px]">
-            <h1 className="w-full text-3xl font-bold text-[#00df9a]">
+            <h1 className="w-full text-5xl font-bold text-[#00df9a]">
                 Action Pack Game
             </h1>
             <ul className="flex">
-                <li className="p-4 cursor-pointer text-blue-500 hover:text-blue-700 transition duration-300" onClick={() => navigateTo('/')}>Home</li>
-                <li className="p-4 cursor-pointer text-blue-500 hover:text-blue-700 transition duration-300" onClick={() => navigateTo('/story')}>Story</li>
-                <li className="p-4 cursor-pointer text-blue-500 hover:text-blue-700 transition duration-300" onClick={() => navigateTo('/characters')}>Characters</li>
-                <li className="p-4 cursor-pointer text-blue-500 hover:text-blue-700 transition duration-300" onClick={() => navigateTo('/about')}>About</li>
+                <li className="p-4 text-lg cursor-pointer text-blue-500 hover:text-blue-700 transition duration-300" onClick={() => navigateTo('/')}>Home</li>
+                <li className="p-4 text-lg cursor-pointer text-blue-500 hover:text-blue-700 transition duration-300" onClick={() => navigateTo('/story')}>Story</li>
+                <li className="p-4 text-lg cursor-pointer text-blue-500 hover:text-blue-700 transition duration-300" onClick={() => navigateTo('/characters')}>Characters</li>
+                <li className="p-4 text-lg cursor-pointer text-blue-500 hover:text-blue-700 transition duration-300" onClick={() => navigateTo('/about')}>About</li>
             </ul>
         </div>
     )
